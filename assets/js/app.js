d5eb74274c12a5a71502e40091387c3e7639a0ec
Michael Putterflam <michael.putterflam@gmail.com>
2019-03-25 23:02:55 -0700

    created HTML and initialized Firebase

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Firebase
+var config = {
+    apiKey: "AIzaSyDGkiWexthigOU0mJMMYGePRkUjViM1bdI",
+    authDomain: "train-scheduler-384f2.firebaseapp.com",
+    databaseURL: "https://train-scheduler-384f2.firebaseio.com",
+    projectId: "train-scheduler-384f2",
+    storageBucket: "",
+    messagingSenderId: "661433660739"
+  };
+  firebase.initializeApp(config);
